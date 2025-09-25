096c6a35cadd43e038c739fe810140eb49d7448d
skantam <sneha.kantam@gmail.com>
2025-09-25 18:11:23 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react', 'pdfjs-dist'],
+  },
+  worker: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
   },
 });
